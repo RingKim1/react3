f8977d3f2516b19083dbdf51e73858640f7a5baa
RingKim1 <kgh9393@naver.com>
2024-05-21 16:11:31 +0900

    feat : ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 export default function App() {
-  const students = [
-    { name: "Alice", age: 17, grade: "A" },
-    { name: "Bob", age: 18, grade: "B" },
-    { name: "Charlie", age: 16, grade: "C" },
-    { name: "Diana", age: 19, grade: "D" },
-  ];
+    const students = [
+        { name: "Alice", age: 17, grade: "A" },
+        { name: "Bob", age: 18, grade: "B" },
+        { name: "Charlie", age: 16, grade: "C" },
+        { name: "Diana", age: 19, grade: "D" },
+    ];
 
-  // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+    // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
+    const filteredStudents = students.filter((el2) => el2.age >= 18);
 
-  return (
-    <div>
-      <h1>학생 목록</h1>
-      <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>학생 목록</h1>
+            <ul>
+                {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+                {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+                {filteredStudents.map((el) => (
+                    <button
+                        key={el.name}
+                        onClick={() => {
+                            alert(`${el.name}(${el.age}), Grade : ${el.grade}`);
+                        }}
+                    >
+                        {el.name}
+                    </button>
+                ))}
+            </ul>
+        </div>
+    );
 }
